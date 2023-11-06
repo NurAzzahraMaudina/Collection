5033c6a32d35d2fcdd21b5a8ba0a762dd032a7d8
NurAzzahraMaudina <146052974+NurAzzahraMaudina@users.noreply.github.com>
2023-11-06 17:30:13 +0800

    Create Splay_tree_set.dart

---
+++ Splay_tree_set.dart
@@ -0,0 +1,13 @@
+import 'dart:collection';
+
+void main(){
+  final scores = SplayTreeMap<String, int>();
+
+  scores['Zahra'] = 100;
+  scores['Dini'] = 100;
+  scores['Bibah'] = 100;
+  scores['Naila'] = 100;
+  scores['Oliv'] = 100;
+
+  print(scores);
+}
