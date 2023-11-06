bf389135201c216180ffbf7096b73910e75af81a
NurAzzahraMaudina <146052974+NurAzzahraMaudina@users.noreply.github.com>
2023-11-06 17:49:06 +0800

    Create Set_method.dart

---
+++ Set_method.dart
@@ -0,0 +1,8 @@
+void main(){
+  final names1 = {"Nur", "Azzahra", "Maudina"};
+  final names2 = {"Bibah", "Cantik", "Banget"};
+
+  print(names1.union(names2));
+  print(names1.intersection(names2));
+  print(names1.difference(names2));
+}
