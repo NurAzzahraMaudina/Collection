209203e9a125c1e08ed74817db6a3ea025478b4a
NurAzzahraMaudina <146052974+NurAzzahraMaudina@users.noreply.github.com>
2023-11-06 17:53:36 +0800

    Create Hash_map.dart

---
+++ Hash_map.dart
@@ -0,0 +1,13 @@
+import 'dart:collection';
+
+void main(){
+  final scores = HashMap<String, int>();
+
+  scores['Bibah'] = 100;
+  scores['Dini'] = 100;
+  scores['Zahra'] = 100;
+  scores['Reina'] = 100;
+  scores['Olip'] = 100;
+
+  print(scores);
+}
